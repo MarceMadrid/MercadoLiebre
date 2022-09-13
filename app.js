60961ffd885d0922b60957bc95660fddd47ec7af
MarceMadrid <marcemadrid1995@gmail.com>
2022-09-12 20:51:17 -0500

    hola

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const path = require("path");
 
 app.use(express.static('public'));
 
-apapp.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo en el puerto 3000")
 });
-
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"./views/home.html"));
